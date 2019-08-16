ad230edfc65614169e08feb57b4760eb426f641f
Sam Curry <sam.curry@readify.net>
2019-08-16 17:05:42 +0800

    add console logging output option (pretty) for local dev output vs JSON stringified output

--- src/logger.config.ts
+++ src/logger.config.ts
@@ -1,26 +1,32 @@
-import { format, TransformableInfo } from 'logform';
+import { format, Format, TransformableInfo } from 'logform';
 import { createLogger, transports } from 'winston';
 import * as Transport from 'winston-transport';
 import config from './config.config';
 
-// tslint:disable: no-var-requires
-const yamlifyObject = require('yamlify-object');
-const yamlifyColors = require('yamlify-object-colors');
-
-const consoleFormat = format.combine(
-    format.colorize(),
-    format.printf((info: TransformableInfo) => {
-        const { level, message, ...meta } = info;
-        const yaml = yamlifyObject(meta, {
-            colors: yamlifyColors,
-        });
-        return `[${level}] ${message}${yaml}`;
-    }),
-);
-
 export const defaultTransports: Transport[] = [];
 
 if (config.get<boolean>('logging.transports.console.enabled')) {
+    let consoleFormat: Format;
+
+    if (config.get<boolean>('logging.transports.console.pretty')) {
+        // tslint:disable: no-var-requires
+        const yamlifyObject = require('yamlify-object');
+        const yamlifyColors = require('yamlify-object-colors');
+
+        consoleFormat = format.combine(
+            format.colorize(),
+            format.printf((info: TransformableInfo) => {
+                const { level, message, ...meta } = info;
+                const yaml = yamlifyObject(meta, {
+                    colors: yamlifyColors,
+                });
+                return `[${level}] ${message}${yaml}`;
+            }),
+        );
+    } else {
+        consoleFormat = format.printf((info: TransformableInfo) => JSON.stringify(info));
+    }
+
     defaultTransports.push(
         new transports.Console({
             format: consoleFormat,
